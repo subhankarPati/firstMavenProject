7a65d9e80f5f65ebfc48f411690a8dd575443cf1
subhankar pati <spati98@example.com>
2018-11-21 12:54:47 +0000

    changes done

--- single-module/src/main/java/com/example/Greeter.java
+++ single-module/src/main/java/com/example/Greeter.java
@@ -11,9 +11,7 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
